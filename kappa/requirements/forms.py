20d0dea6cca66732111aac3056ad2370fb168a48
Luis Fernando Cortes <lfcortes@itc.com.co>
2014-08-06 18:23:56 -0500

    add sort field to search form

--- kappa/requirements/forms.py
+++ kappa/requirements/forms.py
@@ -2,20 +2,24 @@
 from django import forms
 from haystack.forms import SearchForm
 from haystack.inputs import AutoQuery
+import datetime
 
 
 class RequirementSearchForm(SearchForm):
-    q = forms.CharField(label="", max_length=255, required=False, widget=forms.widgets.TextInput(attrs={"class":"form-control",'required': 'true',
+	NEWER = 'desc'
+	OLDER = 'asc'
+	SORT_OPTIONS = (
+				(NEWER , 'Más nuevo'),
+				(OLDER , 'Más viejo')
+	)
+	q = forms.CharField(label="", max_length=255, required=False, widget=forms.widgets.TextInput(attrs={"class":"form-control form-control-large search-input",'required': 'true',
                             'placeholder': 'Buscar ...'}))
-    #my_fieldname = forms.CharField(label="MySearchLabel", max_length=255, required=False)
-
-    def search(self):
-        sqs = super(RequirementSearchForm, self).search()
-
-        #if self.is_valid() and self.cleaned_data['my_fieldname']:
-            #sqs = sqs.filter(my_fieldname=AutoQuery(self.cleaned_data['my_fieldname']))
-
-        return sqs
+	sort = forms.ChoiceField(label="Ordernar por", required=False, choices=SORT_OPTIONS)
+	
+	def search(self):
+		sqs = super(RequirementSearchForm, self).search()
+		sqs = sqs.order_by('pub_created')
+		return sqs
 
 class RequirementCreationForm(forms.Form):
     
