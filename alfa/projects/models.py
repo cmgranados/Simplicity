cda601d04775b9316fdac042cb939b0eb900a761
David Nader <danaderp@itc.com.co>
2014-08-08 08:22:28 -0500

    Se crea el modelo del proyecto Alfa y se ajusta el de kappa.

--- alfa/projects/models.py
+++ alfa/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from shared.types_simplicity.models import Type
+from shared.states_simplicity.models import State
+
+class Project(models.Model):
+    project_id = models.AutoField(primary_key=True)
+    
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        db_table = "af_pr_project"
 
 # Create your models here.
