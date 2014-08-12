3dd234b31eeeb7829ca5afe13b952334db870a54
Luis Fernando Cortes <lfcortes@itc.com.co>
2014-08-12 12:18:41 -0500

    changes to list of requirements form

--- kappa/requirements/forms.py
+++ kappa/requirements/forms.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from haystack.forms import SearchForm
-from haystack.inputs import AutoQuery
-import datetime
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
 
 class RequirementSearchForm(SearchForm):
@@ -12,14 +13,27 @@ class RequirementSearchForm(SearchForm):
 				(NEWER , 'Más nuevo'),
 				(OLDER , 'Más viejo')
 	)
-	q = forms.CharField(label="", max_length=255, required=False, widget=forms.widgets.TextInput(attrs={"class":"form-control form-control-large search-input",'required': 'true',
-                            'placeholder': 'Buscar ...'}))
-	sort = forms.ChoiceField(label="Ordernar por", required=False, choices=SORT_OPTIONS)
+	q = forms.CharField(label="", max_length=255, required=False,)
+	sort = forms.ChoiceField(label='Ordenar por',required=False, choices = SORT_OPTIONS)
 	
 	def search(self):
 		sqs = super(RequirementSearchForm, self).search()
 		sqs = sqs.order_by('pub_created')
 		return sqs
+	
+	# Uni-form
+	helper = FormHelper()
+	helper.form_class = 'form-inline'
+	helper.labels_uppercase = True
+	helper.layout = Layout(
+		Field('q', css_class='input-xlarge'),
+		Field('sort', css_class='pull-right'),
+		FormActions(
+				Submit('save_changes', 'Save changes', css_class="btn-primary"),
+				Submit('cancel', 'Cancel'),
+		)
+		
+	)
 
 class RequirementCreationForm(forms.Form):
     
@@ -27,4 +41,5 @@ class RequirementCreationForm(forms.Form):
     date_created = forms.DateField()
     type = forms.ChoiceField()
     description = forms.Textarea()
+    
     
