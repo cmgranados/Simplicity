a93fcdd1b5cc68d8456a3043f5a07a9d133cc808
Christian Granados <cmgranados@itc.com.co>
2014-08-20 13:23:11 -0500

    Definition for requirement wizard added

--- kappa/static/requirements/js/requirements.js
+++ kappa/static/requirements/js/requirements.js
@@ -1,19 +1,49 @@
-	$( document ).ready(function() {
+$( document ).ready(function() {
 		$.fn.wizard.logging = true;
-		var wizard = $('#satellite-wizard').wizard({
-			keyboard : false,
-						contentHeight : 400,
-						contentWidth : 1000,
-						backdrop: 'static'
-		});
-					
+			var wizard = $('#satellite-wizard').wizard({
+				keyboard : false,
+							contentHeight : 400,
+							contentWidth : 1000,
+							backdrop: 'static'
+			});
+						
 		$('#new-requirement-btn').click(function(e) {
-							e.preventDefault();
-							wizard.show();
-						});
+								e.preventDefault();
+								wizard.show();
+							});
 
 		$('#close-precondition-modal-btn').click(function(e) {
-							$('#myModal').modal('hide')
-						});
-	});
+								$('#myModal').modal('hide')
+							});
 
+		
+
+});
+
+function validateName(el) {
+				var name = el.val();
+				var retValue = {};
+
+				if (name == "") {
+					retValue.status = false;
+					retValue.msg = "Por favor ingrese un nombre";
+				} else {
+					retValue.status = true;
+				}
+
+				return retValue;
+			};
+
+function validateName(el) {
+				var name = el.val();
+				var retValue = {};
+
+				if (name == "") {
+					retValue.status = false;
+					retValue.msg = "Por favor ingrese un nombre";
+				} else {
+					retValue.status = true;
+				}
+
+				return retValue;
+			};
