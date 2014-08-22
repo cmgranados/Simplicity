39e40f44d045bf79c4bf97de4d5de4f903944294
ajardila <ajardila@itc.com.co>
2014-08-22 15:52:51 -0500

    corrected Form No. 5

--- kappa/static/requirements/js/criteria.js
+++ kappa/static/requirements/js/criteria.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var $lastChar =1, $newRow;
 
 	$get_lastID_inputTable = function(){
-		var $id = $('#tableInput tr:last-child td:first-child input').attr("name");
+		var $id = $('#tableCriteria tr:last-child td:first-child input').attr("name");
 	    $lastChar = parseInt($id.substr($id.length - 2), 10);
 	    $lastChar = $lastChar + 1;
 	    $newRow = "<tr> \
@@ -14,23 +14,23 @@ $(document).ready(function(){
     	return $newRow;
 	}
 
-	$("#addRowInput").click(function(){
-	 	if($('#input_table_body tr').length==0){
+	$("#addRowCriteria").click(function(){
+	 	if($('#table_criteria_body tr').length==0){
 			$firstRow = "<tr> \
 				<td><input type='checkbox' name='flow_input_checkbox_01' value=''></td>\
 	            <td><input type='text' name='flow_input_01' maxlength='255' required /></td> \
 	            <td><input type='text' name='flow_input_value_01' maxlength='255' required /></td> \
 	        </tr>"
-			$('#tableInput > tbody:last').append($firstRow)			
+			$('#tableCriteria > tbody:last').append($firstRow)			
 	    } else {
 	    	$get_lastID_inputTable();
-			$('#tableInput > tbody:last').append($newRow);
+			$('#tableCriteria > tbody:last').append($newRow);
 	    };
 		  
 	});
 
-	$("#deleteRowInput").click(function(){
-		$('#tableInput input:checkbox:checked').parents("tr").remove();
+	$("#deleteRowCriteria").click(function(){
+		$('#tableCriteria input:checkbox:checked').parents("tr").remove();
 		$lastChar = $lastChar-2;
 		
 	});
