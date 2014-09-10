990c105b049ec1a386476b7627b4bfa609cf59c5
Luis Fernando Cortes <lfcortes@itc.com.co>
2014-09-10 10:59:49 -0500

    implements validations

--- kappa/static/requirements/js/req_new_requirement.js
+++ kappa/static/requirements/js/req_new_requirement.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	var CONF_VALIDATION = {
 		rules: {
+			requirementTitle: {
+				
+			},
 			requirementType: {
 				required: true,
 				selectcheck: true
@@ -8,7 +11,7 @@ $(document).ready(function() {
 		},
 		messages: {
 			requirementType: {
-				SelectName: "S"
+				
 			}
 		}
 	};
